4eeca72aa26f9dfbb1f99ebd83a3247effa2d838
NadezhdaEr <NadezhdaEr>
2021-12-03 17:50:40 +0300

    - work

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
+
+import com.company.type.Train;
+import com.company.type.train;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("It's alive! It's alive!");
+                Train train1 = new Train("Москва", "2", "30", "26", "10");
+                Train train2 = new Train("СанктПетрбург", "3", "32", "24", "12");
+
+
     }
 }
-
-
---
+++ src/com/company/type/Vokzal.java
@@ -0,0 +1,8 @@
+package com.company.type;
+
+public class Vokzal {
+    Train train;
+    Building building;
+
+
+}
